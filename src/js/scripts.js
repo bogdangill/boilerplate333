4858c3b8fe0e702ebb3be41d412a409a7a561243
Bogdan <herecomesthefog@gmail.com>
2024-01-12 18:03:22 +0300

    add workers

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,9 +1,9 @@
 import { Ripple } from "./ripple.js";
+import Swiper from "swiper";
+import { Pagination, A11y } from "swiper/modules";
 
 const rippleTargetsPrimary = document.querySelectorAll(".pmgt-button--primary[data-ripple='btn']");
 const rippleTargetsOutline = document.querySelectorAll(".pmgt-button--outline[data-ripple='btn']");
-
-console.log(rippleTargetsPrimary);
 
 for (let target of rippleTargetsPrimary) {
     new Ripple(target, {
@@ -11,11 +11,50 @@ for (let target of rippleTargetsPrimary) {
         circleColor: 'default',
         enableForDesktop: true
     });
-}
+};
 for (let target of rippleTargetsOutline) {
     new Ripple(target, {
         circleTag: 'span',
         circleColor: 'primary',
         enableForDesktop: true
     });
-}
+};
+
+let workerTabs = document.querySelectorAll('[data-tab]');
+let workerTabsArr = [];
+
+workerTabs.forEach(tab => workerTabsArr.push(tab.dataset.tab));
+
+let workerTabSlider = new Swiper('.workers-carousel', {
+    modules: [Pagination, A11y],
+    slidesPerView: 1,
+    allowTouchMove: false,
+    pagination: {
+        el: '.workers-carousel__pagination',
+        bulletClass: 'workers-carousel__tab',
+        bulletActiveClass: 'workers-carousel__tab--active',
+        clickable: true,
+        renderBullet: function(index, className) {
+            return `
+                <li class="${className}">
+                    ${workerTabsArr[index]}
+                </li>
+            `;
+        }
+    }
+});
+
+let nannySlider = new Swiper('.workers-slider--nanny', {
+    modules: [A11y],
+    slidesPerView: 'auto',
+    initialSlide: 0
+});
+let housekeeperSlider = new Swiper('.workers-slider--housekeeper', {
+    modules: [A11y],
+    slidesPerView: 'auto',
+    initialSlide: 0
+});
+
+workerTabSlider.init();
+nannySlider.init();
+housekeeperSlider.init();
