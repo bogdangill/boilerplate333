9a998f9345fd4f9957ba4bc77f09327c85f4cc08
Bogdan <herecomesthefog@gmail.com>
2024-01-13 20:26:12 +0300

    refactoring via gulp-data & Nunjucks features

--- gulp-tasks/dev/html.js
+++ gulp-tasks/dev/html.js
@@ -1,9 +1,15 @@
 import gulp from "gulp";
 import nunjucksRender from "gulp-nunjucks-render";
 import browsersync from "browser-sync";
+import data from "gulp-data";
+import * as fs from "node:fs";
 
 export const html = () => {
-    return gulp.src(`./src/html/index.html`) //для лендинга можно только индекс
+    return gulp.src(`./src/html/index.html`) //для лендинга(если помимо него не будет стр) можно только индекс
+        // добавление текстов из json словаря
+        .pipe(data(function() {
+            return JSON.parse(fs.readFileSync('./src/data/data.ru.json'))
+        }))
         .pipe(nunjucksRender({
             path: ["src/html/components/"] //можно и просто строку
         }))
