279ad0845ca7c7abe54313063478aa87bcf913f6
bogdangill <herecomesthefog@gmail.com>
2024-01-17 15:53:09 +0500

    correct for build

--- gulp-tasks/build/html.js
+++ gulp-tasks/build/html.js
@@ -1,8 +1,14 @@
 import gulp from "gulp";
 import nunjucksRender from "gulp-nunjucks-render";
+import data from "gulp-data";
+import * as fs from "node:fs";
 
 export const html = () => {
     return gulp.src("./src/html/index.html")
+        // добавление текстов из json словаря
+        .pipe(data(function() {
+            return JSON.parse(fs.readFileSync('./src/data/data.ru.json'))
+        }))
         .pipe(nunjucksRender({
             path: ["src/html/components/"] //можно и просто строку
         }))
--- gulp-tasks/build/images.js
+++ gulp-tasks/build/images.js
@@ -5,15 +5,15 @@ import imagemin from "gulp-imagemin";
 export const images = () => {
     return gulp.src(`./src/img/**/*.+(png|jpg|gif|ico|svg|webp)`)
         .pipe(webp({
-            quality: 70
+            quality: 90
         }))
         .pipe(gulp.dest(`./build/img/`))
         .pipe(gulp.src(`./src/img/**/*.+(png|jpg|gif|ico|svg|webp)`))
-        .pipe(imagemin({
-            progressive: true,
-            svgoPlugins: [{ removeViewBox: false }],
-            interlaced: true,
-            optimizationLevel: 3 
-        }))
+        // .pipe(imagemin({
+        //     progressive: true,
+        //     svgoPlugins: [{ removeViewBox: false }],
+        //     interlaced: true,
+        //     optimizationLevel: 3 
+        // }))
         .pipe(gulp.dest(`./build/img/`))
 }
